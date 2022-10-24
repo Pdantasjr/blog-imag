29dc3e0d4a607627a40aa84afc3dbb6fb227a1ae
Paulo Dantas <paulodantas19@gmail.com>
2022-10-24 18:30:26 -0300

    Google tag manager

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -33,22 +33,22 @@
         <meta name="facebook-domain-verification" content="zghkuymywarstryl7eb3cmcewcno7c" />
 
         <!-- Google Tag Manager -->
-        <script>
-            (function(w, d, s, l, i) {
-                w[l] = w[l] || [];
-                w[l].push({
-                    'gtm.start': new Date().getTime(),
-                    event: 'gtm.js'
-                });
-                var f = d.getElementsByTagName(s)[0],
-                    j = d.createElement(s),
-                    dl = l != 'dataLayer' ? '&l=' + l : '';
-                j.async = true;
-                j.src =
-                    'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-                f.parentNode.insertBefore(j, f);
-            })(window, document, 'script', 'dataLayer', 'GTM-NXMDS57');
-        </script>
+{{--        <script>--}}
+{{--            (function(w, d, s, l, i) {--}}
+{{--                w[l] = w[l] || [];--}}
+{{--                w[l].push({--}}
+{{--                    'gtm.start': new Date().getTime(),--}}
+{{--                    event: 'gtm.js'--}}
+{{--                });--}}
+{{--                var f = d.getElementsByTagName(s)[0],--}}
+{{--                    j = d.createElement(s),--}}
+{{--                    dl = l != 'dataLayer' ? '&l=' + l : '';--}}
+{{--                j.async = true;--}}
+{{--                j.src =--}}
+{{--                    'https://www.googletagmanager.com/gtm.js?id=' + i + dl;--}}
+{{--                f.parentNode.insertBefore(j, f);--}}
+{{--            })(window, document, 'script', 'dataLayer', 'GTM-NXMDS57');--}}
+{{--        </script>--}}
 
         <!-- Global site tag (gtag.js) - Google Analytics -->
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-PD4XF7X92F"></script>
@@ -62,6 +62,14 @@
 
             gtag('config', 'G-PD4XF7X92F');
         </script>
+
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NXMDS57');</script>
+        <!-- End Google Tag Manager -->
 
         <!-- AdOpt -->
         <meta name="adopt-website-id" content="a5a67294-1dde-424e-abfc-62197de2ab8c" />
@@ -77,5 +85,11 @@
     </style>
     <body class="font-sans antialiased">
         @inertia
+
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NXMDS57"
+                          height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
+
     </body>
 </html>
